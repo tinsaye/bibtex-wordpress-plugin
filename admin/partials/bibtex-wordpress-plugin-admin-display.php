c5717c9dbd41a4ccb220458fd6c638a364e4aef8
tinsaye <abye@studserv.uni-leipzig.de>
2017-12-19 17:20:17 +0100

    init admin interface closes #3

--- admin/partials/bibtex-wordpress-plugin-admin-display.php
+++ admin/partials/bibtex-wordpress-plugin-admin-display.php
@@ -13,4 +13,34 @@
  */
 ?>
 
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
+<div class="wrap">
+
+    <h2><?php echo esc_html(get_admin_page_title()); ?></h2>
+
+    <form method="post" name="bibtex_wpp_options" action="options.php">
+
+    <?php settings_fields($this->plugin_name); ?>
+    
+        <!-- add post,page or product slug class to body class -->
+        <fieldset>
+            <legend class="screen-reader-text">
+            	<span>
+            		<?php _e('Some settings', $this->plugin_name); ?>
+            	</span>
+            </legend>
+            <label for="<?php echo $this->plugin_name; ?>-some_settings">
+                <input type="checkbox" id="<?php echo $this->plugin_name;?>-some_settings" name="<?php echo $this->plugin_name; ?>[some_settings]" value="1" />
+                <span>
+                	<?php esc_attr_e('Some settings', $this->plugin_name); ?>	
+                </span>
+            </label>
+        </fieldset>
+
+        <?php submit_button('Save all changes', 'primary','submit', TRUE); ?>
+
+    </form>
+
+</div>
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
--- includes/class-bibtex-wordpress-plugin.php
+++ includes/class-bibtex-wordpress-plugin.php
@@ -153,10 +153,15 @@ class Bibtex_Wordpress_Plugin {
 	private function define_admin_hooks() {
 
 		$plugin_admin = new Bibtex_Wordpress_Plugin_Admin( $this->get_plugin_name(), $this->get_version() );
+		// Add Settings link to the plugin
+		$plugin_basename = plugin_basename( plugin_dir_path( __DIR__ ) . $this->plugin_name . '.php' );
 
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
-
+		// Add menu item
+		$this->loader->add_action( 'admin_menu', $plugin_admin, 'add_plugin_admin_menu' );
+		// Add Settings link to the plugin
+		$this->loader->add_filter( 'plugin_action_links_' . $plugin_basename, $plugin_admin, 'add_action_links' );
 	}
 
 	/**
